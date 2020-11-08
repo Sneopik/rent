a406e41bdef3e1cb7f7b457074c98d15935c238b
Roman Piskun <sneopik.romka@gmail.com>
2020-11-08 18:31:18 +0300

    type:feature-created a simple page

---
+++ index.js
@@ -0,0 +1,11 @@
+const show_btn = document.querySelector(".navbar-btn");
+const modal_form = document.querySelector(".modal-form");
+const close_form = document.querySelector(".close");
+
+show_btn.addEventListener("click", () => {
+  modal_form.style.display = "block";
+});
+
+close_form.addEventListener("click", () => {
+  modal_form.style.display = "none";
+});
