65aa32b59aa6c4b363df67438bd035bce7e913b0
Roman Piskun <sneopik.romka@gmail.com>
2020-11-08 22:10:26 +0300

    type:fix-created some fixes for modal form

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const close_form = document.querySelector(".close");
 
 show_btn.addEventListener("click", () => {
   modal_form.style.display = "block";
+  setTimeout(() => {
+    modal_form.style.opacity = "1";
+  }, 500);
 });
 
 close_form.addEventListener("click", () => {
-  modal_form.style.display = "none";
+  modal_form.style.opacity = "0";
+  setTimeout(() => {
+    modal_form.style.display = "none";
+  }, 1000);
 });
